84b1ab81a62cd4af5bf90ad80893538c927ea9cc
Oumar Djimé Ratou <oudjira.tc.mi@gmail.com>
2021-02-25 16:32:13 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from app import app
+from app import db
+import views
+from posts.blueprint import posts
+
+app.register_blueprint(posts, url_prefix='/blog')
+# localhost:5000/blog
+
+
+
+if __name__=="__main__":
+    app.run()
